4f89a1fcb96f2d7c160bdefca387e3fef6369e90
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-01-28 20:48:49 +0300

    api: Get API Keys. Create search results model.

---
+++ FinancialApp/Services/APIService.swift
@@ -0,0 +1,15 @@
+//
+//  APIService.swift
+//  FinancialApp
+//
+//  Created by Олег Федоров on 28.01.2022.
+//
+
+import Foundation
+
+struct APIService {
+    
+    let API_KEY = "7VGD3H8O5XWSG1ZY"
+    let API_KEY2 = "ZWS5OSAEJU05SSO2"
+    let API_KEY3 = "XQUMS88NBIJEPZIR"
+}
