a9fc5731063f11009b4786cabc7a37a64f8f0fad
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-01-29 19:32:06 +0300

    api: Add time series monthly adjusted model

---
+++ FinancialApp/Models/TimeSeriesMonthlyAdjusted.swift
@@ -0,0 +1,36 @@
+//
+//  TimeSeriesMonthlyAdjusted.swift
+//  FinancialApp
+//
+//  Created by Олег Федоров on 29.01.2022.
+//
+
+import Foundation
+
+struct TimeSeriesMonthlyAdjusted: Decodable {
+    
+    let metaData: MetaData
+    let timeSeries: [String: OHLC]
+}
+
+struct MetaData: Decodable {
+    
+    let symbol: String
+    
+    enum CodingKeys: String, CodingKey {
+        case symbol = "2. Symbol"
+    }
+}
+
+struct OHLC: Decodable {
+    
+    let open: String
+    let close: String
+    let adjustedClose: String
+    
+    enum CodingKeys: String, CodingKey {
+        case open = "1. open"
+        case close = "4. close"
+        case adjustedClose = "5. adjusted close"
+    }
+}
