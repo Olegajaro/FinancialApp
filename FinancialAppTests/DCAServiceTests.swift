aefbc032708eb167a39296181ac451df6bfbea0a
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-02-08 19:55:41 +0300

    tests: Add test for case when Asset is winning and DCA is used => expect positive gains

--- FinancialAppTests/DCAServiceTests.swift
+++ FinancialAppTests/DCAServiceTests.swift
@@ -23,18 +23,82 @@ class DCAServiceTests: XCTestCase {
         sut = nil
     }
     
+    // test cases
+    // 1. asset = winning | dca = true => positive gains
+    // 2. asset = winning | dca = false => positive gains
+    // 3. asset = losing | dca = true => negative gains
+    // 4. asset = losing | dca = false => negative gains
+    
     // Format for test function name
     // what
     // given
     // expectation
     
     // Dollar Cost Averaging - DCA
-    func testDCAResult_givenDCAIsUsed_expectResult() {
+    func testResult_givenWinningAssetAndDCAIsUsed_expectPositiveGains() {
+        // given
+        let initialInvestmentAmount: Double = 5000
+        let monthlyDollarCostAveragingAmount: Double = 1000
+        let initialDateOfInvestmentIndex = 5 // (6 months ago)
+        let asset = buildWinningAsset()
+        // when
+        let result = sut.calculate(
+            asset: asset,
+            initialInvestmentAmount: initialInvestmentAmount,
+            monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
+            InitialDateOfInvestmentIndex: initialDateOfInvestmentIndex
+        )
+        // then
+        // initial investment: $5000
+        // DCA: $1000 * 5 = $5000
+        // total: $5000 + $5000 = $10000
+        XCTAssertEqual(result.investmentAmount,
+                       10000,
+                       "investment amount is incorrect")
+    }
+    
+    func testResult_givenWinningAssetAndDCAIsNotUsed_expectPositiveGains() {
         
     }
     
-    func testDCAResult_givenDCAIsNotUsed_expectResult() {
+    func testResult_givenLosingAssetAndDCAIsUsed_expectNegativeGains() {
         
+    }
+    
+    func testResult_givenLosingAssetAndDCAIsNotUsed_expectNegativeGains() {
+        
+    }
+    
+    private func buildWinningAsset() -> Asset {
+        let searchResult = buildSearchResult()
+        let metaData = buildMetaData()
+        let timeSeries: [String: OHLC] = [
+            "2021-01-25": OHLC(open: "100", close: "0", adjustedClose: "110"),
+            "2021-02-25": OHLC(open: "110", close: "0", adjustedClose: "120"),
+            "2021-03-25": OHLC(open: "120", close: "0", adjustedClose: "130"),
+            "2021-04-25": OHLC(open: "130", close: "0", adjustedClose: "140"),
+            "2021-05-25": OHLC(open: "140", close: "0", adjustedClose: "150"),
+            "2021-06-25": OHLC(open: "150", close: "0", adjustedClose: "160"),
+        ]
+        let timeSeriesMonthlyAdjusted = TimeSeriesMonthlyAdjusted(
+            metaData: metaData,
+            timeSeries: timeSeries)
+        
+        return Asset(
+            searchResult: searchResult,
+            timeSeriesMonthlyAdjusted: timeSeriesMonthlyAdjusted
+        )
+    }
+    
+    private func buildSearchResult() -> SearchResult {
+        return SearchResult(symbol: "foo",
+                            name: "bar company",
+                            type: "BAZ",
+                            currency: "USD")
+    }
+    
+    private func buildMetaData() -> MetaData {
+        return MetaData(symbol: "foo")
     }
     
     // MARK: - Test getInvestmentAmount method
